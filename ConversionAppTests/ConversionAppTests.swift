689fd5201c30e8a8c81b32ab527655dc2495bc04
Stacey Hongsyvilay <shongsyvilay@gmail.com>
2020-09-25 16:00:25 -0400

    init commit

---
+++ ConversionAppTests/ConversionAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  ConversionAppTests.swift
+//  ConversionAppTests
+//
+//  Created by Pratty A. Hongsyvilay on 9/25/20.
+//  Copyright © 2020 Pratty A. Hongsyvilay. All rights reserved.
+//
+
+import XCTest
+@testable import ConversionApp
+
+class ConversionAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
